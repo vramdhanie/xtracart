f10c58170579f6d082f0807fddf9405238be6222
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-04-17 14:09:48 -0400

    Add firebase hosting and cloud build

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { VERSION } from "./config";
+import { VERSION, BUILD } from "./config";
 
 function App() {
-  return <div className="text-6xl text-center text-blue-300">{VERSION}</div>;
+  return (
+    <div className="text-6xl text-center text-blue-300">
+      {VERSION} {BUILD}
+    </div>
+  );
 }
 
 export default App;
--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
 export const VERSION = process.env.REACT_APP_VERSION;
+export const BUILD = process.env.REACT_APP_BUILD;
