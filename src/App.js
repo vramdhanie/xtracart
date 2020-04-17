ec70ec91d15022757d53dc0e0aafea73d48ade26
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-04-17 19:19:47 -0400

    Testing staging setup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { VERSION, BUILD, GOOGLE_ANALYTICS_KEY } from "./config";
 function App() {
   return (
     <div className="text-6xl text-center text-blue-300">
-      <h1>Preview</h1>
+      <h1>Preview (On Staging)</h1>
       <h3>Version: {VERSION}</h3>
       <h4>Build: {BUILD}</h4>
       <h5>Analytcs: {GOOGLE_ANALYTICS_KEY}</h5>
