5279a1a2eb4dcdc90d1fb1f55538a7afa099f0a5
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-04-20 09:51:28 -0400

    Setup layout of login/register form

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,7 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div> The Login Page</div>;
+  const [login, setLogin] = useState(true);
+  return (
+    <div className="p-2">
+      <div className="w-1/2 mx-auto my-8">
+        <h2 className="font-bold text-xl mb-2 text-gray-700">
+          {login ? "Login" : "Create an account"}
+        </h2>
+        <form>
+          {!login && (
+            <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-first-name"
+                >
+                  First Name
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  id="grid-first-name"
+                  type="text"
+                  placeholder="Jane"
+                />
+                <p className="text-red-500 text-xs italic hidden">
+                  Please fill out this field.
+                </p>
+              </div>
+              <div className="w-full md:w-1/2 px-3">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-last-name"
+                >
+                  Last Name
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-last-name"
+                  type="text"
+                  placeholder="Doe"
+                />
+              </div>
+            </div>
+          )}
+
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-email"
+              >
+                Email
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-email"
+                type="email"
+                placeholder="Your email"
+              />
+              <p className="text-gray-600 text-xs italic"></p>
+            </div>
+          </div>
+
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
+              >
+                Password
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-password"
+                type="password"
+                placeholder="******************"
+              />
+              <p className="text-gray-600 text-xs italic">
+                Make it as long and as crazy as you'd like
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center justify-end py-2">
+            <button
+              className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+              type="submit"
+            >
+              {login ? "Log in" : "Sign Up"}
+            </button>
+            <button
+              className="flex-shrink-0 border-transparent focus:outline-none text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
+              type="button"
+              onClick={() => setLogin((prevLogin) => !prevLogin)}
+            >
+              {login
+                ? "Need to create an account?"
+                : "Already have an account?"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
