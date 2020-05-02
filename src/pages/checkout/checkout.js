e070f8d0bd2443932219c7b993124588ac17faf2
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-05-01 22:59:07 -0400

    Start of store location form

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -1,7 +1,23 @@
 import React from "react";
+import OrderSummary from "../../components/checkout/orderSummary";
+import StoreSelectForm from "../../components/checkout/storeSelectForm";
+import ContactForm from "../../components/checkout/contactForm";
+import PlaceOrder from "../../components/checkout/placeOrder";
 
 const Checkout = () => {
-  return <div className="text-4xl text-blue-400">The checkout page</div>;
+  return (
+    <div className="max-w-5xl mx-auto my-0 p-2 grid grid-cols-1 md:grid-cols-3 gap-2 h-full">
+      <div className="md:col-span-2 p-2">
+        <h2 className="text-xl font-bold border-b border-gray-400 mb-2">
+          Getting your order
+        </h2>
+        <StoreSelectForm />
+        <ContactForm />
+        <PlaceOrder />
+      </div>
+      <OrderSummary />
+    </div>
+  );
 };
 
 export default Checkout;
